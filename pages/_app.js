21eb081a824e42c9153c5fba5acd9ab8b9369a70
Johannes <office@atelierbuchner.at>
2022-09-16 16:41:52 +0200

    cookie consent

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import "../styles/mobile.css";
 import { useState } from "react";
 import { AnimateSharedLayout } from "framer-motion";
 import { CTAContext } from "../components/helper/Content";
+import { CookieBanner } from "@palmabit/react-cookie-law";
+import CookieConsent, { Cookies } from "react-cookie-consent";
 
 function MyApp({ Component, pageProps }) {
     const [ctaOpen, setCTAOpen] = useState();
@@ -11,6 +13,17 @@ function MyApp({ Component, pageProps }) {
     return (
         <AnimateSharedLayout>
             <Component {...pageProps} />;
+            <CookieConsent
+                location="bottom"
+                buttonText="Bestätigen"
+                cookieName="myAwesomeCookieName2"
+                style={{ background: "#414646" }}
+                buttonStyle={{ background: "#d7d0c4", fontSize: "16px" }}
+                expires={150}
+            >
+                Wir verwenden Cookies. Hiermit bestätigen Sie dieses.
+                {/* <span style={{ fontSize: "10px" }}></span> */}
+            </CookieConsent>
         </AnimateSharedLayout>
     );
 }
