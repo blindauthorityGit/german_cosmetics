417c01c137c5f2aba6e73b6e57699804127a5502
Johannes <office@atelierbuchner.at>
2022-09-29 20:28:54 +0200

    startseite tablet

--- components/sections/footer.jsx
+++ components/sections/footer.jsx
@@ -9,7 +9,7 @@ import FormTemplate from "../form/formTemplate";
 
 const Footer = (props) => {
     return (
-        <MainContainer width="w-100 bg-footer px-8 sm:px-0 py-16 py-16 sm:py-32 font-europa mt-16 sm:mt-32">
+        <MainContainer width="w-100 bg-footer px-8 lg:px-0 py-16 py-16 lg:py-32 font-europa mt-16 sm:mt-32">
             <div className="col-span-12 container grid grid-cols-12 m-auto">
                 <div className="col-span-12 sm:col-span-3">
                     <img src={props.logo} alt="Logo" />
@@ -17,16 +17,6 @@ const Footer = (props) => {
                 <FormTemplate></FormTemplate>
 
                 <div className="col-span-12 grid grid-cols-12 mt-16 text-white gap-4">
-                    {/* <div className="col-span-6 sm:col-span-1 socials mt-8 sm:mt-0 order-last sm:order-none">
-                        <div className="flex">
-                            <a className="mr-6" href="https://www.facebook.com/drhubatsch">
-                                <BsFacebook></BsFacebook>
-                            </a>
-                            <a href="https://www.instagram.com/german_aesthetics.de/">
-                                <BsInstagram></BsInstagram>
-                            </a>
-                        </div>
-                    </div> */}
                     <div className="col-span-12 sm:col-span-3 2xl:clamp">
                         <H4 klasse="text-white">Adresse</H4>
                         <span className="opacity-60">
@@ -55,11 +45,7 @@ const Footer = (props) => {
                     <div className="col-span-12 sm:col-span-3 mt-8 sm:mt-0">
                         <H4 klasse="text-white">Praxiszeiten</H4>
                         <div className="wrapper flex oeffnung opacity-60">
-                            <div className="left mr-6 text-left 2xl:clamp">
-                                Mo
-                                <br />
-                                Di
-                                <br />
+                            <div className="left mr-6 text-left  2xl:clamp">
                                 Mi
                                 <br />
                                 Do
@@ -67,7 +53,7 @@ const Footer = (props) => {
                                 Fr
                                 <br />
                             </div>
-                            <div className="right text-left fontAuto">
+                            <div className="right text-left  fontAuto">
                                 <PortableText value={props.value}></PortableText>
                             </div>
                         </div>
