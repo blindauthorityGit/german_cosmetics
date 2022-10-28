82b969597c117554ac00bd2a06f9ec7e831e7070
Johannes <office@atelierbuchner.at>
2022-10-28 13:23:36 +0200

    revalidate

--- components/form/formFull.jsx
+++ components/form/formFull.jsx
@@ -21,7 +21,7 @@ const FormFull = () => {
         let config = {
             method: "post",
             // url: `http://localhost:3000/api/contact`,
-            url: `/api/contact`,
+            url: `./api/contact`,
             headers: {
                 "Content-Type": "application/json",
             },
