e613ff474ab590e9a3cf26932214b9717ff74347
Johannes <office@atelierbuchner.at>
2024-09-12 18:11:11 +0200

    sanity fuckup

---
+++ sanityNew/sanity.config.js
@@ -0,0 +1,24 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+import {structure} from './deskStructure'
+
+export default defineConfig({
+  name: 'default',
+  title: 'german_cosmetics_aestethics',
+
+  projectId: '1tgommzp',
+  dataset: 'production',
+
+  plugins: [
+    structureTool({structure}), // Updated to structureTool for v3
+    ,
+    visionTool(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
