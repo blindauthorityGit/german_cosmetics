dcf14f7019d6c06be636b401bb709cec5474211d
Johannes <office@atelierbuchner.at>
2024-09-17 16:29:50 +0200

    test

--- components/reviews/index.jsx
+++ components/reviews/index.jsx
@@ -9,9 +9,9 @@ const GoogleReviews = ({ reviews }) => {
     }, []);
 
     return (
-        <div className="container mx-auto grid grid-cols-12 w-100   py-16 lg:py-32 font-europa">
+        <div className="container mx-auto grid grid-cols-12 w-100   py-16 lg:pt-32 font-europa">
             <H2 klasse="col-span-12 text-center z-20 text-4xl lg-text-5xl xl:text-6xl 2xl:text-5xl 3xl:text-7xl   font-europa mb-12 text-text">
-                Das sagen unsere Kunden
+                Das sagen unsere Patienten
             </H2>
             {reviews.length > 0 ? (
                 reviews.map(
